265e9ac6b1005e607d7d408f5e909656ec918afb
OdeToCode147 <vamsirao007@gmail.com>
2023-02-23 03:51:23 +0530

    removed versel

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
+const port = 5000;
 
-const alldata = require('./routes/alldata');
-const store = require('./routes/store');
+const alldata = require("./routes/alldata");
+const store = require("./routes/store");
 const app = express();
 
 app.use(cors());
-app.use("/",alldata);
-app.use("/",store);
+app.use("/", alldata);
+app.use("/", store);
 
-app.listen(5000,function(){
-    console.log( "Server is running in port 5000")
-})
+// app.listen(5000,function(){
+//     console.log( "Server is running in port 5000")
+// })
+
+app.listen(process.env.PORT || port, () =>
+  console.log(`Listening on Port ${port}`)
+);
